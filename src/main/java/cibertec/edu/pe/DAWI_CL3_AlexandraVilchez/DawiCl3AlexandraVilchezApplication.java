28f44a214b9b6dbf9b9d178e7050e063a2eef0af
AlexGVP <i202122156@cibertec.edu.pe>
2023-11-26 10:23:38 -0500

    commit inicial

---
+++ src/main/java/cibertec/edu/pe/DAWI_CL3_AlexandraVilchez/DawiCl3AlexandraVilchezApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWI_CL3_AlexandraVilchez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3AlexandraVilchezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3AlexandraVilchezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DAWI_CL3_AlexandraVilchez/DawiCl3AlexandraVilchezApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWI_CL3_AlexandraVilchez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3AlexandraVilchezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
